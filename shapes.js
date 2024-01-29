ce59df446159006b0f9641219d46b0cb27292450
Alex Place <alexander.n.place@gmail.com>
2024-01-29 15:57:47 -0500

    Adding the JavaScript file with the parameters of the 3 different shapes.

---
+++ shapes.js
@@ -0,0 +1,31 @@
+// Creates a class that defines a shape with a color property and a method to set the color.
+class Shape {
+  constructor() {
+    this.color = '';
+  }
+  setColor(color) {
+    this.color = color;
+  }
+}
+// Creates a class to create a circle with specific paramaters and a given color.
+class Circle extends Shape {
+  render() {
+    return `<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${this.color}">`;
+  }
+}
+
+// Creates a class to create a square with specific paramaters and a given color.
+class Square extends Shape {
+  render() {
+    return `<rect x="50" height="200" width="200" fill="${this.color}">`;
+  }
+}
+
+// Creates a class to create a triangle with specific paramaters and a given color.
+class Triangle extends Shape {
+  render() {
+    return `<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.color}">`;
+  }
+}
+
+module.exports = { Circle, Square, Triangle };
